4f243a345c38344131e5c80eb90a587dc72fa8ad
ainurbl <ainur19dota2@mail.ru>
2022-05-13 13:00:57 +0300

    add tcp part

---
+++ hw_12/tcp_part/send.py
@@ -0,0 +1,45 @@
+import datetime
+
+import PySimpleGUI as gui
+
+from util import *
+
+window = gui.Window('TCP send', [
+    [gui.Text('Введите IP для получения', size=DEFAULT_GUI_TEXT_SIZE), gui.InputText(DEFAULT_HOST, key='host')],
+    [gui.Text('Введите порт для получения', size=DEFAULT_GUI_TEXT_SIZE), gui.InputText(DEFAULT_PORT, key='port')],
+    [gui.Text('Введите число пакетов для отправки', size=DEFAULT_GUI_TEXT_SIZE), gui.InputText('500', key='count')],
+    [gui.Button('Отправить')],
+])
+
+while True:
+    event, values = window.read()
+
+    if event in (None, 'Exit'):
+        break
+
+    if event == 'Отправить':
+        host, port, messages_count = None, None, 0
+        try:
+            host, port = values['host'], int(values['port'])
+            messages_count = int(values['count'])
+        except Exception as e:
+            print(e)
+
+        if messages_count == 0:
+            window['count'].Update('Please, enter positive integer')
+            continue
+
+        tcp_socket = None
+        try:
+            tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp_socket.connect((host, port))
+            tcp_socket.sendall(bytes(str(messages_count), encoding='utf-8'))
+            for i in range(messages_count):
+                message = f'{int(datetime.datetime.now().timestamp() * 1000)} '
+                message += random_string(BATCH_SIZE - len(message))
+                tcp_socket.sendto(message.encode(), (host, port))
+        except Exception as e:
+            print(e)
+        finally:
+            if tcp_socket is not None:
+                tcp_socket.close()
